c65d574600eb613e32f9f59c9e4898756addcf56
Ishank Sharma <45262592+ishank-dev@users.noreply.github.com>
2020-02-12 10:42:58 +0530

    Update books.java

--- Semester6/Java-Lab/2/books.java
+++ Semester6/Java-Lab/2/books.java
@@ -1,16 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-
-class bookDatabase{
-	String title,author,publisher,price;
-	bookDatabase(String t,String a,String p,String pr ){
-		title = t;
-		author = a;
-		publisher = p;
-		price = pr;
-	}	
-}
 class bookList
 {	String title,author,publisher,price;
 	bookList(String t,String a,String p,String pr ){
@@ -31,12 +21,12 @@ public class books
 	{	
 		int count = 0;
 		Scanner sc = new Scanner(System.in);
-		ArrayList<bookDatabase> alist = new ArrayList<bookDatabase>();
+		ArrayList<bookList> alist = new ArrayList<bookList>();
 		ArrayList<bookList> blist = new ArrayList<bookList>();
-		alist.add(new bookDatabase("Title 1", "Author 1", "Publisher 1", "Price 1"));
-		alist.add(new bookDatabase("Title 2", "Author 2", "Publisher 2", "Price 2"));
-		alist.add(new bookDatabase("Title 3", "Author 3", "Publisher 3", "Price 3"));
-		alist.add(new bookDatabase("Title 4", "Author 4", "Publisher 4", "Price 4"));
+		alist.add(new bookList("Title 1", "Author 1", "Publisher 1", "Price 1"));
+		alist.add(new bookList("Title 2", "Author 2", "Publisher 2", "Price 2"));
+		alist.add(new bookList("Title 3", "Author 3", "Publisher 3", "Price 3"));
+		alist.add(new bookList("Title 4", "Author 4", "Publisher 4", "Price 4"));
 	
 		while(true){
 			System.out.println("\n1)Add Books\n2)Display Books in normal order\n3)Display Books in sorted order ");
@@ -47,7 +37,7 @@ public class books
 				
 				Random rand = new Random();
 				int randomNumber = rand.nextInt(4);
-				bookDatabase book1 = alist.get(randomNumber);
+				bookList book1 = alist.get(randomNumber);
 				blist.add(new bookList(book1.title,book1.author,book1.publisher,book1.price)); 
 				count++;
 				System.out.println("\nTitle: " + book1.title + "\nAuthor " + book1.author + "\nPublisher" + book1.publisher+"\nPrice" + book1.price + "\nAdded successfully");
@@ -61,13 +51,13 @@ public class books
 				}
 				break;
 			
-				case 3:
-				ArrayList<bookList> blist1 = new ArrayList<bookList>(blist);
-				Collections.sort(blist);
-				for(int i = 0; i < blist.size(); i++)
-				{	
-					System.out.println(blist.get(i));
-				}
+				// case 3:
+				// ArrayList<bookList> blist1 = new ArrayList<bookList>(blist);
+				// Collections.sort(blist);
+				// for(int i = 0; i < blist.size(); i++)
+				// {	
+				// 	System.out.println(blist.get(i));
+				// }
 
 			}
 		}
